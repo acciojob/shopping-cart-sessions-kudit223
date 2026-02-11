cd0b223a4915d3fc252247d4fa14deeb0c734a1c
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2026-02-11 14:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,29 +9,59 @@ const products = [
   { id: 5, name: "Product 5", price: 50 },
 ];
 
+
+
 // DOM elements
 const productList = document.getElementById("product-list");
-
+const cartProductlist=document.querySelector('#cart-list');
+const clearCartbtn=document.querySelector('#clear-cart-btn')
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" onClick=addToCart(${product.id}) data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+    let sessionStorageCart=JSON.parse(sessionStorage.getItem('inCart'));
+    cartProductlist.innerHTML='';
+    if(!sessionStorageCart) return;
+    sessionStorageCart.forEach((item)=>{
+        const li=document.createElement('li');
+        li.innerHTML+=`${item.name} - $${item.price}`;
+        cartProductlist.appendChild(li);
+    })
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+    let inCart=[];
+    let userSelectedProduct=products[productId-1];
+    let sessionStorageCart=JSON.parse(sessionStorage.getItem('inCart'));
+
+    if(sessionStorageCart){
+        sessionStorageCart.forEach(item=>{
+        inCart.push(item);
+    })}
+    
+    inCart.push(userSelectedProduct);
+    sessionStorage.setItem('inCart',JSON.stringify(inCart))
+    renderCart();
+}
 
 // Remove item from cart
 function removeFromCart(productId) {}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+    sessionStorage.clear();
+    renderCart();
+}
+
+clearCartbtn.addEventListener('click',clearCart)
 
 // Initial render
 renderProducts();
